e3a85ab571f150738b27c65f661ea197a55103e5
zhangyayun <zhangyy@greatld.com>
2019-10-11 14:07:03 +0800

    fix bug

--- middleware/session.js
+++ middleware/session.js
@@ -1,28 +1,29 @@
-var redisclient = require('../dao/redisClient');
 const uuidv4 = require('uuid/v4');
-var sessionmiddle={};
+var sessionmiddle = {};
+const util = require('../src/util');
+const SessionExpire = 3600;
+const tokenExpire = 3600;
 
-sessionmiddle.newSessionId = function(){
+sessionmiddle.newSessionId = function () {
 	return uuidv4();
 };
 
-sessionmiddle.sessionName = function(){
+sessionmiddle.sessionName = function () {
 	return '_node_session';
 };
 
-sessionmiddle.getSessionId = function(cookeie,sessionkey){
+sessionmiddle.getSessionId = function (cookeie, sessionkey) {
 	// console.log(' in cookeie ',cookeie,sessionkey)
 	let sessionid = null;
-	if(cookeie&&cookeie.length>0)
-	{
+	if (cookeie && cookeie.length > 0) {
 		// debugger;
-		try{
+		try {
 			let cookeies = cookeie.split(';');
-			cookeies.forEach(function(item){
-				if(item.split('=')[0].trim() == sessionkey)
+			cookeies.forEach(function (item) {
+				if (item.split('=')[0].trim() == sessionkey)
 					sessionid = item.split('=')[1];
 			});
-		}catch(e){
+		} catch (e) {
 			console.error(e);
 		}
 	}
@@ -30,81 +31,93 @@ sessionmiddle.getSessionId = function(cookeie,sessionkey){
 	return sessionid;
 };
 
-sessionmiddle.middle = function(req, res,next){
+sessionmiddle.middle = function (req, res, next) {
 	// console.log('req.session',req.session)
 	// console.log("Cookies: ", req.get('cookie'));
-	let sessionid = sessionmiddle.getSessionId(req.get('cookie'),sessionmiddle.sessionName());
-	if(!sessionid){
+	let token = req.method == "GET" ? req.query.token : req.body.token; //支持token方式
+	let sessionid = token || (sessionmiddle.getSessionId(req.get('cookie'), sessionmiddle.sessionName()));
+	if (!sessionid) {
 		sessionid = sessionmiddle.newSessionId();
-		res.cookie(sessionmiddle.sessionName(),sessionid);
-		
+		res.cookie(sessionmiddle.sessionName(), sessionid, {
+			httpOnly: true
+		}); //maxAge: 900000,
 	}
-	 new session(sessionid,redisclient,function(session){
-	 	req.session = session;
+	new session(sessionid, !!token, function (session) {
+		req.session = session;
 		next();
 	});
-	
+
 };
 
 
-let session = function(sessionid,redisclient,cb){
+let session = function (sessionid, isToken, cb) {
 	let _session = {};
 	let me = this;
-	sessionid = '_session'+sessionid;
-	this.get = function(){
+	sessionid = '_session' + sessionid;
+	this.get = function () {
 		return _session;
 	}
 
-	this.update = function(){
-		if(!_session)
+	this.update = function (expiretimespan) {
+		if (!_session)
 			return;
-		redisclient.set(sessionid,JSON.stringify(_session),function(err){
-			if(err)
-				console.error('session update error ',err);
-			me.expire();
+		if (!expiretimespan)
+			expiretimespan = isToken ? tokenExpire : SessionExpire;
+		util.redis.putValue({
+			key: sessionid,
+			value: JSON.stringify(_session),
+			seconds: expiretimespan
+		}).then((err) => {
+			// if(err&&err!='OK')
+			// 	console.error('session update error ',err);
+		})
+	};
+
+	this.expire = function (expiretimespan) {
+		if (!expiretimespan)
+			expiretimespan = isToken ? tokenExpire : SessionExpire;
+		util.redis.exec({
+			cmd: 'expire',
+			argv: [sessionid, expiretimespan],
+			dbIndex: 0
 		});
 	};
 
-	this.expire = function(expiretimespan){
-		if(!expiretimespan)
-			expiretimespan = 18e2;
-		redisclient.connect(function(client){
-			client.expire(sessionid, expiretimespan);
-		});
-	};
-
-	this.clear = function(cb){
-		redisclient.connect(function(client){
-			client.del(sessionid,function(err, response){
-				if (response == 1) {
-			      console.log("Deleted Successfully!")
-			   } else{
-			    console.log("Cannot delete")
-			   }
-			   if(cb)
-			   	cb(err,response);
-			});
+	this.clear = function (cb) {
+		util.redis.delKey({
+			key: sessionid
+		}).then((err, response) => {
+			if (err && err != 'OK')
+				console.log("Cannot delete")
+			if (cb)
+				cb(err, response);
 		});
 	}
 
-	function getSessionValuefRedis(sessionid,cb){
-		// console.log('sessionid',sessionid);
-		redisclient.get(sessionid,function(err,value){ 
-			if(err)
-				return console.error('session get error ',err);
-			// console.log('value',value);
-			if(value)
+	this.newSession = (sessionId) => {
+		sessionid = '_session' + sessionId;
+	}
+
+	function getSessionValuefRedis(sessionid, cb) {
+		util.redis.getKeyAndTtl({
+			key: sessionid
+		}).then(({
+			ttl,
+			value
+		}) => {
+			if (value)
 				_session = JSON.parse(value);
 			else
 				_session = {};
 			// console.log(value,_session);
-			if(cb)
+			if (cb)
 				cb(me);
-		});
-		me.expire();
+			if (!ttl > -1)
+				me.expire();
+		})
 	}
 
-	getSessionValuefRedis(sessionid,cb);
+	getSessionValuefRedis(sessionid, cb);
 
 };
 
--- src/util.js
+++ src/util.js
@@ -2,22 +2,29 @@
 const Promise = require('bluebird');
 const uuid = require('uuid');
 const common = require('./common');
-const crypto=require('crypto');
+const crypto = require('crypto');
+var redis = require('../lib/redis');
+var _redisCONTANCe = redis.connect();
 
 let util = {
-    get UUID(){
-  		return uuid.v4().replace(/-/g, '');
-    },
-    get now(){
-        return Math.round(new Date().getTime()/1000);
-    },
-    get nowStr(){
-      return new Date().format('yyyy-MM-dd hh:mm:ss');
-    }
+  get UUID() {
+    return uuid.v4().replace(/-/g, '');
+  },
+  get now() {
+    return Math.round(new Date().getTime() / 1000);
+  },
+  get nowStr() {
+    return new Date().format('yyyy-MM-dd hh:mm:ss');
+  },
+  get redis() {
+    if (!_redisCONTANCe)
+      _redisCONTANCe = redis.connect()
+    return _redisCONTANCe;
+  }
 };
 
-util.md5 = (str)=>{
-  let md5=crypto.createHash("md5");
+util.md5 = (str) => {
+  let md5 = crypto.createHash("md5");
   md5.update(str);
   return md5.digest('hex');
 }
@@ -29,187 +36,211 @@ util.md5 = (str)=>{
  * @returns {*}
  */
 let getQueryConnection = (sql, schemasNo = 1) => {
-    let dmlType = sql.trim().substring(0, 6).toLowerCase();
-    if (dmlType === 'insert' || dmlType === 'update' || dmlType === 'delete'){
-      //连接写数据库
-      if (schemasNo === 1){
-        //qichacha(write)
-        return common.MSQLInstance;
-      } 
-    } else {
-      //连接读数据库
-      if (schemasNo === 1){
-        //qichacha(read)
-        return common.MSQLInstance;
-      } 
+  let dmlType = sql.trim().substring(0, 6).toLowerCase();
+  if (dmlType === 'insert' || dmlType === 'update' || dmlType === 'delete') {
+    //连接写数据库
+    if (schemasNo === 1) {
+      //qichacha(write)
+      return common.MSQLInstance;
     }
-  };
-  
-  /**
-   * 执行SQL
-   * Create by Zhangyayun
-   *
-   * @param sql 需要执行的sql语句
-   * @param values sql参数
-   * @param schemasNo 需要连接的数据库。1：qichacha、2：qichacha_extend, 3: qcc_data_user_re  默认1
-   * @returns {*}
-   */
-  util.query = (sql, values, schemasNo = 1) => {
-    return getQueryConnection(sql, schemasNo).query(sql, values);
-  };
-  
-  
-  /*
-  * 批量执行sql，尽量模拟事务操作，如果都是写语句或者都是读语句可以保证事务性，如果是读写混合的语句，那么会分成两个事务
-  * Create by Zhangyayun
-  *
-  * @param querys 批量语句，形式有两种选择
-  * 1,[item...]                       串行顺序执行，所以速度会慢一些，使用Promise.mapSeries实现
-  * 2,{query1:item,query2:item2...}   无序并行执行，使用Promise.props实现
-  * 两种参数形式中的 item 同样还有两种形式 [sql,values] OR {sql: sql, values: values} 效果是一样的，推荐第一种数组的形式
-  *
-  * @param schemasNo 需要连接的数据库。1：qichacha、2：qichacha_extend, 3: qcc_data_user_re  默认1
-  *
-  * @returns Promise, resolve结果形式与参数querys有关,querys=>数组，那么returns=>数组
-  * */
- util.queryTranstions = (querys, schemasNo = 1)=>{
-  
-    let writeAbleConnect = null;
-    let readAbleConnect = null;
-  
-    for (let i in querys) {
-      let query = querys[i];
-      let sql = null;
-      if (Array.isArray(query))
-        sql = query[0];
-      else if (typeof  query === 'object')
-        sql = query.sql;
-      else
-        continue;
-  
-      let dmlType = sql.trim().substring(0, 6).toLowerCase();
-      if (!writeAbleConnect&&(dmlType === 'insert' || dmlType === 'update' || dmlType === 'delete'))
-        writeAbleConnect = getQueryConnection(sql, schemasNo);
-      if (!readAbleConnect && dmlType === 'select')
-        readAbleConnect = getQueryConnection(sql, schemasNo);
+  } else {
+    //连接读数据库
+    if (schemasNo === 1) {
+      //qichacha(read)
+      return common.MSQLInstance;
     }
-  
-    //由pool实例获取transtion连接
-    let getTransConnect = (wcon, rcon)=>{
-      if (wcon&&rcon)
-        return wcon.trans().then((wConn)=>{
-          return rcon.getConn().then((reConn)=>{
-            return {'wConn': wConn, 'rConn': reConn};
-          });
-        });
-      if (wcon&&!rcon)
-        return wcon.trans().then((conn)=>{
-          return {'wConn': conn, 'rConn': null};
-        });
-      if (!wcon&&rcon)
-        return rcon.getConn().then((conn)=>{
-          return {'wConn': null, 'rConn': conn};
-        });
-      return Promise.resolve({'wConn': null, 'rConn': null});
-    };
-  
-    //根据sql语句选择连接
-    let choseConnect = (sql, wcon, rcon)=>{
-      let dmlType = sql.trim().substring(0, 6).toLowerCase();
-      return dmlType === 'select'? rcon : wcon;
-    };
-  
-    //提交并释放连接
-    let commit = (wConn, rConn)=>{
-      return new Promise((resolve, reject)=>{
-        if (wConn)
-          wConn.commit((err)=>{
-            if (err) {
-              console.log('commit异常，释放连接',err);
-              releaseWithRollBack(wConn,rConn);
-              return reject(err);
-            }
-            wConn.release();
-            resolve(true);
-          });
-        else
-          resolve(true);
-      });
-    };
-  
-    //回滚并释放连接
-    let releaseWithRollBack = (wConn, rConn)=>{
-      if(wConn) {
-        wConn.rollback();
-        wConn.release();
-      }
-    };
-  
-    //释放连接
-    let release = (wConn, rConn)=>{
-      if(wConn)
-        wConn.release();
-      // if(rConn)
-      //   rConn.release();
-    };
-  
-    return getTransConnect(writeAbleConnect, readAbleConnect).then(({wConn, rConn})=>{
-      //有序执行
-      if (Array.isArray(querys))
-        return Promise.mapSeries(querys, (query)=>{
-          let {sql, values} = query;
-          if (Array.isArray(query))
-            [sql, values] = query;
-          return choseConnect(sql, wConn, rConn).queryAsync({sql: sql, values: values});
-        }).then(result=>{
-          return commit(wConn, rConn).then(()=>result);
-        }).catch(err=>{
-          releaseWithRollBack(wConn,rConn);
-          console.log('异常，释放连接',err);
-          return Promise.reject(err);
-        });
-  
-      //无序执行
-      let queryPromise = {};
-      for (let p in querys){
-        let {sql, values} = querys[p];
-        if (Array.isArray(querys[p]))
-          [sql, values] = querys[p];
-        queryPromise[p] = choseConnect(sql, wConn, rConn).queryAsync({sql: sql, values: values});
-      }
-  
-      return Promise.props(queryPromise).then(result=>{
-        return commit(wConn, rConn).then(()=>result);
-      }).catch(err=>{
-        releaseWithRollBack(wConn,rConn);
-        console.log('异常，释放连接',err);
-        return Promise.reject(err);
-      });
-    });
-  };
-  
+  }
+};
 
-  Date.prototype.format = function(format){
-   var o = {
-   "M+" : this.getMonth()+1, //month
-   "d+" : this.getDate(),    //day
-   "h+" : this.getHours(),   //hour
-   "m+" : this.getMinutes(), //minute
-   "s+" : this.getSeconds(), //second
-   "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
-   "S" : this.getMilliseconds() //millisecond
-   }
-   if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
-   (this.getFullYear()+"").substr(4 - RegExp.$1.length));
-   for(var k in o)if(new RegExp("("+ k +")").test(format))
-   format = format.replace(RegExp.$1,
-   RegExp.$1.length==1 ? o[k] :
-   ("00"+ o[k]).substr((""+ o[k]).length));
-   return format;
+/**
+ * 执行SQL
+ * Create by Zhangyayun
+ *
+ * @param sql 需要执行的sql语句
+ * @param values sql参数
+ * @param schemasNo 需要连接的数据库。1：qichacha、2：qichacha_extend, 3: qcc_data_user_re  默认1
+ * @returns {*}
+ */
+util.query = (sql, values, schemasNo = 1) => {
+  return getQueryConnection(sql, schemasNo).query(sql, values);
+};
+
+
+/*
+ * 批量执行sql，尽量模拟事务操作，如果都是写语句或者都是读语句可以保证事务性，如果是读写混合的语句，那么会分成两个事务
+ * Create by Zhangyayun
+ *
+ * @param querys 批量语句，形式有两种选择
+ * 1,[item...]                       串行顺序执行，所以速度会慢一些，使用Promise.mapSeries实现
+ * 2,{query1:item,query2:item2...}   无序并行执行，使用Promise.props实现
+ * 两种参数形式中的 item 同样还有两种形式 [sql,values] OR {sql: sql, values: values} 效果是一样的，推荐第一种数组的形式
+ *
+ * @param schemasNo 需要连接的数据库。1：qichacha、2：qichacha_extend, 3: qcc_data_user_re  默认1
+ *
+ * @returns Promise, resolve结果形式与参数querys有关,querys=>数组，那么returns=>数组
+ * */
+util.queryTranstions = (querys, schemasNo = 1) => {
+
+  let writeAbleConnect = null;
+  let readAbleConnect = null;
+
+  for (let i in querys) {
+    let query = querys[i];
+    let sql = null;
+    if (Array.isArray(query))
+      sql = query[0];
+    else if (typeof query === 'object')
+      sql = query.sql;
+    else
+      continue;
+
+    let dmlType = sql.trim().substring(0, 6).toLowerCase();
+    if (!writeAbleConnect && (dmlType === 'insert' || dmlType === 'update' || dmlType === 'delete'))
+      writeAbleConnect = getQueryConnection(sql, schemasNo);
+    if (!readAbleConnect && dmlType === 'select')
+      readAbleConnect = getQueryConnection(sql, schemasNo);
   }
 
-  module.exports =  util;
+  //由pool实例获取transtion连接
+  let getTransConnect = (wcon, rcon) => {
+    if (wcon && rcon)
+      return wcon.trans().then((wConn) => {
+        return rcon.getConn().then((reConn) => {
+          return {
+            'wConn': wConn,
+            'rConn': reConn
+          };
+        });
+      });
+    if (wcon && !rcon)
+      return wcon.trans().then((conn) => {
+        return {
+          'wConn': conn,
+          'rConn': null
+        };
+      });
+    if (!wcon && rcon)
+      return rcon.getConn().then((conn) => {
+        return {
+          'wConn': null,
+          'rConn': conn
+        };
+      });
+    return Promise.resolve({
+      'wConn': null,
+      'rConn': null
+    });
+  };
+
+  //根据sql语句选择连接
+  let choseConnect = (sql, wcon, rcon) => {
+    let dmlType = sql.trim().substring(0, 6).toLowerCase();
+    return dmlType === 'select' ? rcon : wcon;
+  };
+
+  //提交并释放连接
+  let commit = (wConn, rConn) => {
+    return new Promise((resolve, reject) => {
+      if (wConn)
+        wConn.commit((err) => {
+          if (err) {
+            console.log('commit异常，释放连接', err);
+            releaseWithRollBack(wConn, rConn);
+            return reject(err);
+          }
+          wConn.release();
+          resolve(true);
+        });
+      else
+        resolve(true);
+    });
+  };
+
+  //回滚并释放连接
+  let releaseWithRollBack = (wConn, rConn) => {
+    if (wConn) {
+      wConn.rollback();
+      wConn.release();
+    }
+  };
+
+  //释放连接
+  let release = (wConn, rConn) => {
+    if (wConn)
+      wConn.release();
+    // if(rConn)
+    //   rConn.release();
+  };
+
+  return getTransConnect(writeAbleConnect, readAbleConnect).then(({
+    wConn,
+    rConn
+  }) => {
+    //有序执行
+    if (Array.isArray(querys))
+      return Promise.mapSeries(querys, (query) => {
+        let {
+          sql,
+          values
+        } = query;
+        if (Array.isArray(query))
+          [sql, values] = query;
+        return choseConnect(sql, wConn, rConn).queryAsync({
+          sql: sql,
+          values: values
+        });
+      }).then(result => {
+        return commit(wConn, rConn).then(() => result);
+      }).catch(err => {
+        releaseWithRollBack(wConn, rConn);
+        console.log('异常，释放连接', err);
+        return Promise.reject(err);
+      });
+
+    //无序执行
+    let queryPromise = {};
+    for (let p in querys) {
+      let {
+        sql,
+        values
+      } = querys[p];
+      if (Array.isArray(querys[p]))
+        [sql, values] = querys[p];
+      queryPromise[p] = choseConnect(sql, wConn, rConn).queryAsync({
+        sql: sql,
+        values: values
+      });
+    }
+
+    return Promise.props(queryPromise).then(result => {
+      return commit(wConn, rConn).then(() => result);
+    }).catch(err => {
+      releaseWithRollBack(wConn, rConn);
+      console.log('异常，释放连接', err);
+      return Promise.reject(err);
+    });
+  });
+};
 
 
+Date.prototype.format = function (format) {
+  var o = {
+    "M+": this.getMonth() + 1, //month
+    "d+": this.getDate(), //day
+    "h+": this.getHours(), //hour
+    "m+": this.getMinutes(), //minute
+    "s+": this.getSeconds(), //second
+    "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
+    "S": this.getMilliseconds() //millisecond
+  }
+  if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+    (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(format))
+      format = format.replace(RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] :
+        ("00" + o[k]).substr(("" + o[k]).length));
+  return format;
+}
 
-
+module.exports = util;
